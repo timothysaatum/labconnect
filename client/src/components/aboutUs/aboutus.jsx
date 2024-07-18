e713c599fee79ff9951126f887dced902138ad52
debfevlo <deborahfevlo@gmail.com>
2024-07-18 16:25:39 +0000

    Update aboutus.jsx
    changes made

--- client/src/components/aboutUs/aboutus.jsx
+++ client/src/components/aboutUs/aboutus.jsx
@@ -3,23 +3,8 @@ import React from 'react';
 export default function AboutUs(){
     
     return(
-    
-        <div className="bg-gray-50 min-h-screen flex items-center justify-center px-16">
-  <div className="relative w-full max-w-lg">
-    <div className="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
-    <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
-    <div className="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
-    <div className="m-8 relative space-y-4">
-      <div className="p-5 bg-white rounded-lg flex items-center justify-between space-x-8 w-">
-        <div>
-          
-        </div>
-        <div>
-          
-        </div>
-      </div>
+    <div>
+        <h1>hello world </h1>
     </div>
-  </div>
-</div>
     )
 }
