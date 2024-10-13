2eca467b6059a9ee810cfd16c46888a43918228f
timothysaatum <timothysaatum@outlook.com>
2024-10-13 15:21:46 +0000

    fixed sample referrals

--- sample/serializers.py
+++ sample/serializers.py
@@ -1,25 +1,55 @@
 from rest_framework import serializers # type: ignore
-from sample.models import Sample, Notification, SampleTrackingHistory, Referral
+from sample.models import (
+      Sample, 
+      Notification, 
+      SampleTrackingHistory, 
+      Referral,
+      SampleTest
+      )
 from labs.models import Test
 from modelmixins.models import Facility, SampleType
 from modelmixins.serializers import SampleTypeSerializer
 
 
+class SampleTestSerializer(serializers.ModelSerializer):
+
+    sample = serializers.PrimaryKeyRelatedField(
+        queryset=Sample.objects.all(), required=True
+    )
+    test = serializers.PrimaryKeyRelatedField(
+        queryset=Test.objects.all(), required=True
+    )
+    status = serializers.CharField(required=False)
+    is_emmergency = serializers.BooleanField(default=False)
+    result = serializers.URLField(required=False)
+
+
+    class Meta:
+
+        model = SampleTest
+
+        fields = (
+            "id",
+            "sample",
+            "test",
+            "status",
+            'result',
+            "is_emmergency",
+            "date_completed",
+        )
+
+
 class SampleSerializer(serializers.ModelSerializer):
 
-    tests = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Test.objects.all(), required=True
-    )
-    attachment = serializers.URLField(required=False)
     referral = serializers.PrimaryKeyRelatedField(
         queryset=Referral.objects.all(), required=False
     )
     sample_type = serializers.PrimaryKeyRelatedField(
         queryset=SampleType.objects.all(), required=False
     )
-    is_emmergency = serializers.BooleanField(default=False)
     rejection_reason = serializers.CharField(required=False)
     sample_status = serializers.CharField(required=False)
+    sample_tests = SampleTestSerializer(many=True)
 
     class Meta:
 
@@ -28,22 +58,60 @@ class SampleSerializer(serializers.ModelSerializer):
         fields = (
             "id",
             "sample_type",
-            "tests",
+            "sample_tests",
             "referral",
-            "attachment",
             "sample_status",
-            "is_emmergency",
             "rejection_reason",
             "date_modified",
-            "date_added",
+            "date_collected",
         )
+
+    def create(self, validated_data):
+        sample_tests_data = validated_data.pop('sample_tests')
+        sample = Sample.objects.create(**validated_data)
+
+        # Creating SampleTest entries
+        for sample_test_data in sample_tests_data:
+            SampleTest.objects.create(sample=sample, **sample_test_data)
+
+        return sample
+
+    def update(self, instance, validated_data):
+        sample_tests_data = validated_data.pop("sample_tests")
+        instance.patient_name = validated_data.get(
+            "patient_name", instance.patient_name
+        )
+        instance.patient_age = validated_data.get("patient_age", instance.patient_age)
+        instance.patient_sex = validated_data.get("patient_sex", instance.patient_sex)
+        instance.clinical_history = validated_data.get(
+            "clinical_history", instance.clinical_history
+        )
+        instance.requires_phlebotomist = validated_data.get(
+            "requires_phlebotomist", instance.requires_phlebotomist
+        )
+        instance.sample_status = validated_data.get(
+            "sample_status", instance.sample_status
+        )
+        instance.save()
+
+        for sample_test_data in sample_tests_data:
+            sample_test = SampleTest.objects.filter(
+                sample=instance, test=sample_test_data["test"]
+            ).first()
+            if sample_test:
+                sample_test.status = sample_test_data.get("status", sample_test.status)
+                sample_test.rejection_reason = sample_test_data.get(
+                    "rejection_reason", sample_test.rejection_reason
+                )
+                sample_test.save()
+            else:
+                SampleTest.objects.create(sample=instance, **sample_test_data)
+
+        return instance
 
     def to_representation(self, instance):
 
         data = super().to_representation(instance)
-
-        data["tests"] = [test.name for test in instance.tests.all()]
-
         data["referral"] = str(instance.referral)
         data["sample_type"] = SampleTypeSerializer(
             SampleType.objects.filter(sample=instance), many=True
@@ -65,9 +133,7 @@ class ReferralSerializer(serializers.ModelSerializer):
     referring_facility = serializers.PrimaryKeyRelatedField(
         queryset=Facility.objects.all(), required=True
     )
-    samples = serializers.PrimaryKeyRelatedField(
-        required=False, queryset=Sample.objects.all(), many=True
-    )
+    samples = SampleSerializer(many=True)
     facility_type = serializers.CharField(read_only=True)
 
     class Meta:
@@ -91,16 +157,74 @@ class ReferralSerializer(serializers.ModelSerializer):
             "date_referred",
         )
 
+    def create(self, validated_data):
+        samples_data = validated_data.pop("samples")
+        referral = Referral.objects.create(**validated_data)
+
+        # Creating Sample entries
+        for sample_data in samples_data:
+            sample_tests_data = sample_data.pop("sample_tests")
+            sample = Sample.objects.create(referral=referral, **sample_data)
+
+            # Creating SampleTest entries for each sample
+            for sample_test_data in sample_tests_data:
+                SampleTest.objects.create(sample=sample, **sample_test_data)
+
+        return referral
+
+
+    def update(self, instance, validated_data):
+        samples_data = validated_data.pop("samples")
+        instance.referring_facility = validated_data.get(
+            "referring_facility", instance.referring_facility
+        )
+        instance.to_laboratory = validated_data.get(
+            "to_laboratory", instance.to_laboratory
+        )
+        instance.save()
+
+        for sample_data in samples_data:
+            sample = Sample.objects.filter(
+                referral=instance, id=sample_data["id"]
+            ).first()
+            if sample:
+                sample.patient_name = sample_data.get(
+                    "patient_name", sample.patient_name
+                )
+                sample.save()
+
+                # Update sample tests
+                for sample_test_data in sample_data["sample_tests"]:
+                    sample_test = SampleTest.objects.filter(
+                        sample=sample, test=sample_test_data["test"]
+                    ).first()
+                    if sample_test:
+                        sample_test.status = sample_test_data.get(
+                            "status", sample_test.status
+                        )
+                        sample_test.rejection_reason = sample_test_data.get(
+                            "rejection_reason", sample_test.rejection_reason
+                        )
+                        sample_test.save()
+            else:
+                # Create new sample and associated tests
+                new_sample = Sample.objects.create(referral=instance, **sample_data)
+                for sample_test_data in sample_data["sample_tests"]:
+                    SampleTest.objects.create(sample=new_sample, **sample_test_data)
+
+        return instance
+
     def to_representation(self, instance):
-        print("Hello")
+
         data = super().to_representation(instance)
-        print('Hello')
-        print(Sample.objects.filter(referral=instance), 'hi')
-        data["samples"] = SampleSerializer(
-                Sample.objects.filter(referral=instance), many=True
-            ).data
+
+        # print(Sample.objects.filter(referral=instance), 'hi')
+        # data["samples"] = SampleSerializer(
+        #         Sample.objects.filter(referral=instance), many=True
+        #     ).data
         data["referring_facility"] = str(instance.referring_facility)
         data["to_laboratory"] = str(instance.to_laboratory)
+
         return data
 
 
